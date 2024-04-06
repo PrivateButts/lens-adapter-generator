cd32512d4fb87b8198e9f261adc242ea9003c000
Randy Butts <randybuttsii@gmail.com>
2024-04-06 02:33:13 -0400

    Improved voron plate generation

---
+++ public/scad/voron/attribution.md
@@ -0,0 +1,3 @@
+These files are based off of rdmullett and oldfartt's work:
+
+https://github.com/rdmullett/voron_serial_plate
--- src/views/VoronGenerator.vue
+++ src/views/VoronGenerator.vue
@@ -14,11 +14,18 @@
       />
       <div class="label">
         <span class="label-text-alt">
-          The current plate model accommodates up to 7 characters properly
+          The current plate models accommodate up to 7 characters properly
         </span>
       </div>
     </label>
     <div class="form-control">
+      <label class="label cursor-pointer">Extrusion Size</label>
+      <select class="select select-bordered" v-model="extrusionSize">
+        <option value="2020">2020 (Voron 2.4)</option>
+        <option value="1515">1515 (Voron 0)</option>
+      </select>
+    </div>
+    <div class="form-control my-2">
       <label class="label cursor-pointer">
         <span class="label-text">Include Logo:</span>
         <input type="checkbox" v-model="options.logo" class="checkbox checkbox-primary" />
@@ -31,26 +38,42 @@
       :scad-variables="options"
       :model-options="modelOptions"
     />
+    <p class="mt-3 italic text-sm">
+      These plates are based on the work of
+      <a class="link" href="https://github.com/rdmullett/voron_serial_plate" target="_blank"
+        >rdmullett</a
+      >
+      and
+      <a class="link" href="https://www.thingiverse.com/thing:4947518" target="_blank">oldfartt</a>.
+    </p>
   </form>
 </template>
 
 <script setup lang="ts">
 import GeneratorForm from '@/components/GeneratorForm.vue'
-import { reactive, computed } from 'vue'
+import { reactive, computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 
-const scadFile = { url: 'scad/voron/Voron_Logo_Plate.scad', name: 'plate.scad' }
+const extrusionSize = ref('2020')
+
+const scadFile = { url: 'scad/voron/voron.scad', name: 'plate.scad' }
 const resources = [
-  { url: 'scad/voron/Serial_Plate_Voron_Logo.stl', name: 'Serial_Plate_Voron_Logo.stl' },
-  { url: 'scad/voron/Serial_Plate_Voron_NoLogo.stl', name: 'Serial_Plate_Voron_NoLogo.stl' },
+  { url: 'scad/voron/2020logo.stl', name: '2020logo.stl' },
+  { url: 'scad/voron/2020logo7.stl', name: '2020logo7.stl' },
+  { url: 'scad/voron/2020nologo.stl', name: '2020nologo.stl' },
+  { url: 'scad/voron/2020nologo7.stl', name: '2020nologo7.stl' },
+  { url: 'scad/voron/1515logo.stl', name: '1515logo.stl' },
+  { url: 'scad/voron/1515nologo.stl', name: '1515nologo.stl' },
   { url: 'scad/voron/Play.ttf', name: 'fonts/Play.ttf' }
 ]
+const use1515 = computed(() => extrusionSize.value === '1515')
 const options = reactive({
   font1: 'Play',
   serial: route.query.serial || 'VX.XXXX',
-  logo: true
+  logo: true,
+  use1515: use1515
 })
 const modelOptions = reactive({
   color: '#cc000e'
